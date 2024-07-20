99ec21208a99e16920c22607ad146a6a26d7ebf4
Adryellsz <adryellpaulo@gmail.com>
2024-07-20 15:06:09 -0300

    channels

--- 101/goroutine.go
+++ 101/goroutine.go
@@ -5,22 +5,36 @@ import (
 	"time"
 )
 
-func goroutine_numeros(){
+// Channel só escreve
+func goroutine_numeros(done chan <- bool){
 	for l := 'a'; l < 'j'; l++{
 		fmt.Printf("%c", l);
 		time.Sleep(time.Millisecond * 150)
 	}
+
+	done <- true
 }
 
-func goroutine_letras(){
+// Channel só escreve
+func goroutine_letras(done chan <- bool){
 	for n := 1; n < 10; n++{
 		fmt.Println(n);
 		time.Sleep(time.Millisecond * 250)
 	}
+
+	done <- true
 }
 
 func goroutines(){
-	go goroutine_letras()
-	go goroutine_numeros()
-	time.Sleep(5 * time.Second) // Pro código nao morrer antes da execução das goroutines
+	channel_numeros := make(chan bool)
+	channel_letras := make(chan bool)
+
+	go goroutine_letras(channel_letras)
+	go goroutine_numeros(channel_numeros)
+
+	<- channel_letras
+	<- channel_numeros
+
+	 //time.Sleep(5 * time.Second) // Pro código nao morrer antes da execução das goroutines // A abordagem funciona mas é melhor usar channels
+
 }
